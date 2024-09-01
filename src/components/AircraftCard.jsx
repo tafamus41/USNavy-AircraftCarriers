33ffae34cc5442fc1f4ca77e7bcc14b982bee117
asimov <seyranmustafa4@gmail.com>
2024-09-01 20:25:22 +0300

    p

--- src/components/AircraftCard.jsx
+++ src/components/AircraftCard.jsx
@@ -1,10 +1,21 @@
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 
-const AircraftCard = ({img,name,characteristics}) => {
-    console.log(name);
+const AircraftCard = ({ img, name, characteristics }) => {
+//   console.log(name);
   return (
-    <div>AircraftCard</div>
-  )
-}
+    <Col>
+      <Card >
+        <Card.Img variant="top" src={img} />
+        <Card.Body>
+          <Card.Title>{name}</Card.Title>
+          <Button variant="primary">Characteristics</Button>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
-export default AircraftCard
+export default AircraftCard;
